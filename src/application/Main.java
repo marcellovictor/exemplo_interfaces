8a4911b146651ff3cbfd4e2aefdff21f239fc3c5
Marcello Victor <marcello.victor.ferreira@hotmail.com>
2020-09-10 00:42:37 -0300

    DateFormats e Instanciando Rental

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,12 @@
 package application;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Rental;
+import util.DateFormats;
 
 public class Main {
 
@@ -11,9 +16,30 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter rental data");
+		System.out.print("Car model: ");
+		String model = sc.nextLine();
+		
+		Date pickup = new Date();
+		System.out.print("Pickup (dd/MM/yyyy hh:mm): ");
+		try {
+			pickup = DateFormats.full.parse(sc.nextLine());
+		} 
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		Date returno = new Date();
+		System.out.print("Return (dd/MM/yyyy hh:mm): ");
+		try {
+			returno = DateFormats.full.parse(sc.nextLine());
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
 		
-		
+		Rental rental = new Rental(model, pickup, returno);
 		
 		sc.close();
 	}
